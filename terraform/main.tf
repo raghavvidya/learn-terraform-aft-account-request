33751b2aba2abd7d3cfdbff8a85c502d3ca82a50
raghavvidya <raghavvidya@gmail.com>
2025-01-04 22:49:50 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "raghavvidyasandbox@gmail.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "raghavvidyadevops@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
